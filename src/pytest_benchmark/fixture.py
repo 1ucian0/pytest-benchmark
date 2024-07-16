5eb5d27a357590babf05ba5db9b2c174999761f2
Luciano Bello <bel@zurich.ibm.com>
2024-07-16 17:56:48 +0200

    force method fork

--- src/pytest_benchmark/fixture.py
+++ src/pytest_benchmark/fixture.py
@@ -165,7 +165,8 @@ class BenchmarkFixture:
             pass  # "No {self.skipfile}"
 
     def _check_timeout(self, function_to_benchmark, args, kwargs):
-        from wrapt_timeout_decorator import timeout
+        from wrapt_timeout_decorator import timeout, set_subprocess_starting_method
+        set_subprocess_starting_method('fork')
         func = timeout(dec_timeout=self.timeout_skip_list, use_signals=False)(function_to_benchmark)
         try:
             func(*args, **kwargs)
